f98780bedf2ac65245d141b06ec1093607eaf0af
davi schilling <davischilling.dev@gmail.com>
2021-08-10 21:33:32 +0200

    add typescript

---
+++ src/main/config/module-alias.ts
@@ -0,0 +1,7 @@
+import { addAlias } from "module-alias";
+import { resolve } from "path";
+
+addAlias('@', resolve('dist'))
+
+/* Importar este arquivo no arquivo
+// inicial da aplicação (@/main/api/index.ts) */
