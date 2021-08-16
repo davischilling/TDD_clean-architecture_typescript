7d712229ae804dce1987856de04879e40051d4d9
davi schilling <davischilling.dev@gmail.com>
2021-08-16 10:57:44 +0200

    ensure AxiosHttpClient rethrows if get throws

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -44,6 +44,14 @@ describe('AxiosHttpClient', () => {
 
       expect(axiosResult).toEqual('any_data')
     })
+
+    it('should rethrow if get throws', async () => {
+      fakeAxios.get.mockRejectedValueOnce(new Error('http_error'))
+
+      const promise = sut.get({ url, params })
+
+      await expect(promise).rejects.toThrow(new Error('http_error'))
+    })
   })
   // describe('post', () => {
   //   it('', () => {
