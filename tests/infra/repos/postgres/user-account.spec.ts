7b25de675ffe43907b7f2cc4a29a1c59bd74611d
davi schilling <davischilling.dev@gmail.com>
2021-08-17 12:21:51 +0200

    ensure PgUserAccountRepository loads an account if email exists

---
+++ tests/infra/repos/postgres/user-account.spec.ts
@@ -0,0 +1,87 @@
+import { LoadUserAccountRepository, SaveFacebookAccoutRepository } from '@/data/contracts/repos'
+
+import { newDb, IMemoryDb } from 'pg-mem'
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  getRepository,
+  Connection,
+  Repository
+} from 'typeorm'
+
+class PgUserAccountRepository implements LoadUserAccountRepository {
+  async load (params: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
+    const pgUserRepo = getRepository(PgUser)
+    const pgUser = await pgUserRepo.findOne({ email: params.email })
+    if (pgUser !== undefined) {
+      return {
+        id: pgUser.id.toString(),
+        name: pgUser.name ?? undefined
+      }
+    }
+  }
+
+  async saveWithFacebook (params: SaveFacebookAccoutRepository.Params): Promise<void> {
+
+  }
+}
+
+@Entity({ name: 'usuarios' })
+export class PgUser {
+  @PrimaryGeneratedColumn()
+  id!: number
+
+  @Column({ name: 'nome', nullable: true })
+  name?: string
+
+  @Column()
+  email!: string
+
+  @Column({ name: 'id_facebook', nullable: true })
+  facebookId?: string
+}
+
+describe('PgUserAccountRepository', () => {
+  let sut: PgUserAccountRepository
+  let db: IMemoryDb
+  let connection: Connection
+  let pgUserRepo: Repository<PgUser>
+
+  beforeAll(async () => {
+    db = newDb()
+    connection = await db.adapters.createTypeormConnection({
+      type: 'postgres',
+      entities: [PgUser]
+    })
+    await connection.synchronize()
+    pgUserRepo = getRepository(PgUser)
+  })
+
+  beforeEach(async () => {
+    await pgUserRepo.save({ email: 'existing_email' })
+    sut = new PgUserAccountRepository()
+  })
+
+  afterEach(async () => {
+    await pgUserRepo.clear()
+  })
+
+  afterAll(async () => {
+    await connection.close()
+  })
+
+  describe('load', () => {
+    it('should return an account if email exists', async () => {
+      const account = await sut.load({ email: 'existing_email' })
+
+      expect(account).toEqual({ id: '1' })
+    })
+  })
+
+  // describe('saveWithFacebook', () => {
+  //   it('should save user with correct params', () => {
+
+  //   })
+  // })
+})
