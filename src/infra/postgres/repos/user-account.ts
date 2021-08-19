12880a83236b2ca5d003aa9fd2bb725b336542ca
davi schilling <davischilling.dev@gmail.com>
2021-08-19 18:39:07 +0200

    add facebookLogin integration tests

--- src/infra/postgres/repos/user-account.ts
+++ src/infra/postgres/repos/user-account.ts
@@ -9,10 +9,9 @@ type FacebookSaveParams = SaveFacebookAccoutRepository.Params
 type FacebookSaveResult = SaveFacebookAccoutRepository.Result
 
 export class PgUserAccountRepository implements LoadUserAccountRepository, SaveFacebookAccoutRepository {
-  private readonly pgUserRepo = getRepository(PgUser)
-
   async load ({ email }: LoadParams): Promise<LoadResult> {
-    const pgUser = await this.pgUserRepo.findOne({ email })
+    const pgUserRepo = getRepository(PgUser)
+    const pgUser = await pgUserRepo.findOne({ email })
     if (pgUser != null) {
       return {
         id: pgUser.id.toString(),
@@ -24,12 +23,13 @@ export class PgUserAccountRepository implements LoadUserAccountRepository, SaveF
   }
 
   async saveWithFacebook ({ id, email, name, facebookId }: FacebookSaveParams): Promise<FacebookSaveResult> {
+    const pgUserRepo = getRepository(PgUser)
     let resultId: string
     if (id === undefined) {
-      const pgUser = await this.pgUserRepo.save({ email, name, facebookId })
+      const pgUser = await pgUserRepo.save({ email, name, facebookId })
       resultId = pgUser.id.toString()
     } else {
-      await this.pgUserRepo.update({ id: parseInt(id) }, { name, facebookId })
+      await pgUserRepo.update({ id: parseInt(id) }, { name, facebookId })
       resultId = id
     }
     return { id: resultId }
---
+++ tests/main/routes/login.spec.ts
@@ -0,0 +1,53 @@
+import { UnauthorizedError } from '@/application/errors'
+import { PgUser } from '@/infra/postgres/entities'
+import { app } from '@/main/config/app'
+import { FakeDb, makeFakeDb } from '@/tests/infra/postgres/mocks'
+
+import request from 'supertest'
+
+describe('Login Routes', () => {
+  let pg: FakeDb
+  const loadUserSpy = jest.fn()
+
+  jest.mock('@/infra/apis/facebook', () => ({
+    FacebookApi: jest.fn().mockReturnValue({
+      loadUser: loadUserSpy
+    })
+  }))
+
+  beforeAll(async () => {
+    pg = await makeFakeDb([PgUser])
+  })
+
+  beforeEach(async () => {
+    pg.backup.restore()
+  })
+
+  afterAll(async () => {
+    await pg.connection.close()
+  })
+
+  describe('POST /login/facebook', () => {
+    it('should return 200 with AccessToken', async () => {
+      loadUserSpy.mockResolvedValueOnce({
+        facebookId: 'any_id', name: 'any_name', email: 'any_email'
+      })
+
+      const { status, body } = await request(app)
+        .post('/api/login/facebook')
+        .send({ token: 'valid_token' })
+
+      expect(status).toBe(200)
+      expect(body.accessToken).toBeDefined()
+    })
+
+    it('should return 401 with AnauthorizedError', async () => {
+      const { status, body } = await request(app)
+        .post('/api/login/facebook')
+        .send({ token: 'invalid_token' })
+
+      expect(status).toBe(401)
+      expect(body.error).toEqual(new UnauthorizedError().message)
+    })
+  })
+})
