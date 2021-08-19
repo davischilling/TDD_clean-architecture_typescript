476ead047190550027ec592f6ef248c6dca04f87
davi schilling <davischilling.dev@gmail.com>
2021-08-19 16:53:13 +0200

    ensure ExpressRouter calls handle with correct request

---
+++ tests/infra/http/express-router.spec.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express'
+import { getMockReq, getMockRes } from '@jest-mock/express'
+import { mock, MockProxy } from 'jest-mock-extended'
+import { Controller } from '@/application/controllers'
+
+class ExpressRouter {
+  constructor (
+    private readonly controller: Controller
+  ) {}
+
+  async adapt (req: Request, res: Response): Promise<void> {
+    await this.controller.handle({ ...req.body })
+  }
+}
+
+describe('ExpressRouter', () => {
+  let sut: ExpressRouter
+  let req: Request
+  let res: Response
+  let controller: MockProxy<Controller>
+
+  beforeAll(() => {
+    (req = getMockReq({ body: { any: 'any' } }));
+    ({ res } = getMockRes())
+    controller = mock()
+  })
+
+  beforeEach(() => {
+    sut = new ExpressRouter(controller)
+  })
+
+  it('should call handle with correct request', async () => {
+    await sut.adapt(req, res)
+
+    expect(controller.handle).toHaveBeenCalledWith({ any: 'any' })
+  })
+})
